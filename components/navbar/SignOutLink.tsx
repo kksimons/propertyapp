25145d5043828a77499cb983f7a582b3fa475b14
kksimons <kksimons@gmail.com>
2024-07-13 17:38:41 -0600

    Sign in, Sign out implemented

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+'use client';
+
+import { SignOutButton } from '@clerk/nextjs';
+import { useToast } from '../ui/use-toast';
 
 function SignOutLink() {
+  const { toast } = useToast();
+  const handleLogout = () => {
+    toast({ description: 'You have been signed out.' });
+  };
   return (
-    <div>
-      Signout
-    </div>
-  )
+    <SignOutButton redirectUrl='/'>
+      <button className='w-full text-left' onClick={handleLogout}>
+        Logout
+      </button>
+    </SignOutButton>
+  );
 }
-
-export default SignOutLink
+export default SignOutLink;
