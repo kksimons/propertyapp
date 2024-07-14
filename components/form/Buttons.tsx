cb565af0aebb311238e2712a4e28be6baf0cc02e
kksimons <kksimons@gmail.com>
2024-07-13 18:09:39 -0600

    create profile setup

---
+++ components/form/Buttons.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useFormStatus } from 'react-dom';
+import { Button } from '@/components/ui/button';
+import { AiOutlineReload } from "react-icons/ai";
+
+'use client'
+
+type SubmitButtonProps = {
+    className?: string;
+    text?: string;
+  };  
+
+export function SubmitButton({className='', text='submit'}:SubmitButtonProps) {
+    const {pending} = useFormStatus()
+
+    return <Button type='submit' disabled={pending} className={`capitalize ${className}`} size='lg'>
+      {pending ? (
+        <>
+          <AiOutlineReload className='mr-2 h-4 w-4 animate-spin' />
+          Please wait...
+        </>
+      ) : (
+        text
+      )}
+    </Button>
+}
