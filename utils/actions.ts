97ef6f2218cdce07b00219898bf7cd051919098c
kksimons <kksimons@gmail.com>
2024-07-14 12:54:43 -0600

    added createProfileAction

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,15 +1,35 @@
 'use server'
 
+import db from './db';
+import { clerkClient, currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 import { profileSchema } from "./schemas"
 
 export const createProfileAction = async (prevState: any, formData: FormData) => {
     try {
-        const rawData = Object.fromEntries(formData)
-        const validatedFields = profileSchema.parse(rawData)
-        console.log(validatedFields)
-        return {message: 'profile created'}
+        const user = await currentUser()
+        // console.log(user)
+        if(!user) throw new Error('there was an error thanks to typescript')
+            const rawData = Object.fromEntries(formData)
+            const validatedFields = profileSchema.parse(rawData)
+            // console.log(validatedFields)
+        await db.profile.create({
+            data: {
+                clerkId: user.id,
+                email: user.emailAddresses[0].emailAddress,
+                profileImage: user.imageUrl ?? '',
+                ...validatedFields,
+            }
+        })
+        await clerkClient.users.updateUserMetadata(user.id, {
+            privateMetadata: {
+                hasProfile: true,
+            }
+        })
+        // return {message: 'profile created'}
     } catch (error) {
         console.log(error)
-        return {message: 'there was an error'}
+        return {message: error instanceof Error? error.message: 'Something bad happened'}
     }
+    redirect('/')
 }
