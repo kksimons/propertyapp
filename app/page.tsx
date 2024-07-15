778a8be35688fc7ba1274d55c5d7f2cdf2f4a6b8
kksimons <kksimons@gmail.com>
2024-07-15 16:23:22 -0600

    finished skeleton frames

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,28 @@
 import CategoriesList from '@/components/home/CategoriesList';
 import PropertiesContainer from '@/components/home/PropertiesContainer';
 import { Button } from '@/components/ui/button';
+import LoadingCards from '@/components/card/LoadingCards';
+import { Suspense } from 'react';
+
 
 function HomePage({
   searchParams,
-}:{
-  searchParams:{category?: string, search?: string}
+}: {
+  searchParams: { category?: string; search?: string };
 }) {
-  console.log(searchParams)
-
   return (
     <section>
-      <CategoriesList category={searchParams.category} search={searchParams.search} />
-      <PropertiesContainer category={searchParams.category} search={searchParams.search} />
+      <CategoriesList
+        category={searchParams?.category}
+        search={searchParams?.search}
+      />
+      {/* we need to fetch inside the properties container */}
+      <Suspense fallback={<LoadingCards />}>
+        <PropertiesContainer
+          category={searchParams?.category}
+          search={searchParams?.search}
+        />
+      </Suspense>
     </section>
   );
 }
